9d686c85c2d1c13b65809306576dbab38d9a121a
crams <cpramsey112@gmail.com>
2023-07-09 13:02:58 -0400

    models sync

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// Import required packages
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -19,6 +18,8 @@ mongoose.connect('mongodb://localhost/social_network_db', {
 })
   .then(() => {
     console.log('Connected to the MongoDB database');
+    // Sync the models with the database
+    syncModels();
     // Start the server after successful database connection
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
@@ -27,3 +28,32 @@ mongoose.connect('mongodb://localhost/social_network_db', {
   .catch((error) => {
     console.error('Error connecting to the MongoDB database:', error);
   });
+
+// Import the models
+const User = require('./models/User');
+const Thought = require('./models/Thought');
+const Reaction = require('./models/Reaction');
+
+// Sync the models with the database
+const syncModels = async () => {
+  try {
+    await User.syncIndexes();
+    await Thought.syncIndexes();
+    await Reaction.syncIndexes();
+    console.log('Models synced with the database');
+  } catch (error) {
+    console.error('Error syncing models with the database:', error);
+  }
+};
+
+// Import and use the route files
+const usersRoutes = require('./routes/users');
+const thoughtsRoutes = require('./routes/thoughts');
+const reactionsRoutes = require('./routes/reactions');
+
+app.use(usersRoutes);
+app.use(thoughtsRoutes);
+app.use(reactionsRoutes);
+
+
+
